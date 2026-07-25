83dfee6fe782797549180f6f8f4685ae1e9f677f
Elijah Johnson <elijohnson626@gmail.com>
2026-07-24 22:59:13 -0400

    Change

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import routeRoutes from "./routes/routeRoutes.js";
+import runRoutes from "./routes/runRoutes.js";
 
 const app = express();
 
@@ -34,7 +35,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/routes", routeRoutes);
 app.use("/api/user", userRoutes);
-
+app.use("/api/runs", runRoutes);
 console.log("✅ All Express Routes Loaded");
 
 // 4. Debug 404 Handler
